672b43c4de13b043b80e5c701da89961a6613e1f
dmitrii <dima_agr@icloud.com>
2023-09-07 16:24:10 +0300

    Initial commit

---
+++ src/main/java/oop2/Main.java
@@ -0,0 +1,28 @@
+package oop2;
+
+public class FirstMain {
+    public static void main(String[] args) {
+        SmallCar smallCar = new SmallCar();
+        System.out.println("\n" + smallCar.doorsQuantity());
+        System.out.println(smallCar.model());
+        System.out.println(smallCar.bodyType());
+        System.out.println(smallCar.color());
+
+        BigCar bigCar = new BigCar();
+        System.out.println("\n" + bigCar.doorsQuantity());
+        System.out.println(bigCar.model());
+        System.out.println(bigCar.bodyType());
+        System.out.println(bigCar.color());
+
+        Cars cars = new Cars() {
+            @Override
+            public int doorsQuantity() {
+                return 4;
+            }
+        };
+        System.out.println("\n" + cars.doorsQuantity());
+        System.out.println(cars.bodyType());
+
+    }
+
+}
